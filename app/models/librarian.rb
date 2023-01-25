e5e417e60d339962af5c8a754b72de289c4e96a2
Abhinandana <abhinandana@nunify.com>
2023-01-25 16:42:51 +0530

    Setting up librarians in the system with name,email and password

---
+++ app/models/librarian.rb
@@ -0,0 +1,7 @@
+class Librarian < ApplicationRecord
+  before_save {self.email =email.downcase}
+  validates :name ,presence:true ,length:{minimum:4,maximum: 25},uniqueness: { case_sensitive: false }
+  EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email ,presence:true ,length:{minimum:10,maximum: 100},uniqueness: { case_sensitive: false },format: {with:EMAIL_REGEX}
+  validates :password,presence: true
+end
