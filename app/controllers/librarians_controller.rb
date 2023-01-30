f6d64e26bd6049da72bf0ea0c6f00014fc94bb43
Abhinandana <abhinandana@nunify.com>
2023-01-30 17:07:20 +0530

    Librarians can see a list of all books that the library has

--- app/controllers/librarians_controller.rb
+++ app/controllers/librarians_controller.rb
@@ -1,6 +1,7 @@
 class LibrariansController < ApplicationController
   def show
     @librarian=Librarian.find(params[:id])
+    @books=@librarian.books.all
   end
   def index
     @librarians=Librarian.all
